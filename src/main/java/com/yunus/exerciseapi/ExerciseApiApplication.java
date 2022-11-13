c5ae95d6971425ee2373a573e31811ec169efaa6
yunussezgin <yunussezgin8@hotmail.com>
2022-11-13 12:49:31 +0100

    Create project

---
+++ src/main/java/com/yunus/exerciseapi/ExerciseApiApplication.java
@@ -0,0 +1,13 @@
+package com.yunus.exerciseapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExerciseApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExerciseApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yunus/exerciseapi/ExerciseApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yunus.exerciseapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExerciseApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
